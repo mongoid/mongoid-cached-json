bf0e631dd1aad5b29458469cd143760839b40f71
dblock <dblock@dblock.org>
2013-01-17 20:54:09 -0500

    Renamed variables to avoid confusion with Hash::keys.

--- lib/mongoid-cached-json/hash.rb
+++ lib/mongoid-cached-json/hash.rb
@@ -1,23 +1,23 @@
 class Hash
 
   def as_json_partial(options = {})
-    keys = nil
+    json_keys = nil
     json = inject({}) do |h, (k, v)|
       if v.respond_to?(:as_json_partial)
-        partial_keys, partial_json = v.as_json_partial(options)
-        keys = keys ? keys.merge_set(partial_keys) : partial_keys
+        partial_json_keys, partial_json = v.as_json_partial(options)
+        json_keys = json_keys ? json_keys.merge_set(partial_json_keys) : partial_json_keys
         h[k] = partial_json
       else
         h[k] = v.as_json(options)
       end
       h
     end
-    [ keys, json ]
+    [ json_keys, json ]
   end
 
   def as_json(options = {})
-    keys, json = as_json_partial(options)
-    Mongoid::CachedJson.materialize_json_references_with_read_multi(keys, json)
+    json_keys, json = as_json_partial(options)
+    Mongoid::CachedJson.materialize_json_references_with_read_multi(json_keys, json)
   end
 
 end
