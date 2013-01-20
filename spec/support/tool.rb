65bc5688b3b38c85cc7f4bd3ed62cfff1732731c
dblock <dblock@dblock.org>
2013-01-20 11:52:40 -0500

    Added missing models for benchmark tests.

---
+++ spec/support/tool.rb
@@ -0,0 +1,12 @@
+class Tool
+  include Mongoid::Document
+  include Mongoid::CachedJson
+
+  field :name
+  belongs_to :tool_box
+
+  json_fields \
+    :name => {},
+    :tool_box => { :type => :reference }
+
+end
